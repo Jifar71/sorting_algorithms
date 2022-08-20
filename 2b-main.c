173e2f66e3d5e92ba7f2eb18e85bcabb8c7da843
Jifar71 <jifarraya3@gmail.com>
2022-08-20 19:38:18 +0000

    commit sorting

---
+++ 2b-main.c
@@ -0,0 +1,20 @@
+#include "sort.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    size_t n = 1001;
+	int *array = rand_array(n, 100);
+
+    print_array(array, n);
+    printf("\n");
+    selection_sort(array, n);
+    printf("\n");
+    print_array(array, n);
+    check_array(array, n);
+    return (0);
+}
